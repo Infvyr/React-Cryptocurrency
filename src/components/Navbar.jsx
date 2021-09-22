781aadba44802625a85e76a952b5ea038eca318e
Infvyr <allfortrashmail@gmail.com>
2021-09-22 21:37:46 +0300

    fixed navbar and other app general files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,51 @@
-import { Button, Menu, Typography, Avatar } from "antd";
-import { Link } from "react-router-dom";
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
 import {
-  HomeOutlined,
-  MoneyCollectOutlined,
-  BulbOutlined,
-  FundOutlined,
-  MenuOutlined,
-} from "@ant-design/icons";
+	HomeOutlined,
+	MoneyCollectOutlined,
+	BulbOutlined,
+	FundOutlined,
+	MenuOutlined,
+	ReadOutlined,
+} from '@ant-design/icons';
 
-import icon from "../images/cryptocurrency.svg";
+import icon from '../images/cryptocurrency.svg';
 
 const Navbar = () => {
-  return (
-    <header className="nav-container">
-      <div className="logo-container">
-        <Avatar src={icon} size="large" />
-        <Typography.Title level={2} className="logo">
-          <Link to="/">Crypto App</Link>
-        </Typography.Title>
-      </div>
-      <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
-    </header>
-  );
+	const menuItems = [
+		{ id: 1, icon: <HomeOutlined />, url: '/', title: 'Home' },
+		{
+			id: 2,
+			icon: <FundOutlined />,
+			url: '/cryptocurrencies',
+			title: 'Cryptocurrencies',
+		},
+		{
+			id: 3,
+			icon: <MoneyCollectOutlined />,
+			url: '/exchanges',
+			title: 'Exchanges',
+		},
+		{ id: 4, icon: <ReadOutlined />, url: '/news', title: 'News' },
+	];
+
+	return (
+		<header className="nav-container">
+			<div className="logo-container">
+				<Avatar src={icon} size="large" />
+				<Typography.Title level={2} className="logo">
+					<Link to="/">Crypto App</Link>
+				</Typography.Title>
+			</div>
+			<Menu theme="dark">
+				{menuItems.map(item => (
+					<Menu.Item icon={item.icon} key={item.id}>
+						<Link to={item.url}>{item.title}</Link>
+					</Menu.Item>
+				))}
+			</Menu>
+		</header>
+	);
 };
 
 export default Navbar;
