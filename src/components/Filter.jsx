c68e26abdd6f6dcee8f1e2c3eb4f035d8f133e3e
Infvyr <allfortrashmail@gmail.com>
2021-09-28 22:05:57 +0300

    news filter

---
+++ src/components/Filter.jsx
@@ -0,0 +1,68 @@
+import { Col, Space, Select } from 'antd';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const { Option } = Select;
+
+const Filter = ({ handleSelectNews, handleSortNews, handleOrderNews }) => {
+	const { data } = useGetCryptosQuery(100);
+
+	return (
+		<Col span={24}>
+			<Space size="small">
+				<Select
+					className="select-news"
+					placeholder="Select a category"
+					optionFilterProp="children"
+					showSearch
+					onChange={handleSelectNews}
+					filterOption={(inputValue, option) =>
+						option.children.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0
+					}>
+					<Option value="Cryptocurrency">All</Option>
+					{data?.data?.coins.map(coin => (
+						<Option value={coin.name} key={coin.id}>
+							{coin.name}
+						</Option>
+					))}
+				</Select>
+
+				<Select
+					style={{ width: 200 }}
+					placeholder="Sort by"
+					optionFilterProp="children"
+					onChange={handleSortNews}
+					filterOption={(input, option) =>
+						option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+					}
+					filterSort={(optionA, optionB) =>
+						optionA.children
+							.toLowerCase()
+							.localeCompare(optionB.children.toLowerCase())
+					}>
+					<Option value="relevance">Relevance</Option>
+					<Option value="date">Date</Option>
+				</Select>
+
+				<Select
+					style={{ width: 200 }}
+					placeholder="Order by"
+					optionFilterProp="children"
+					onChange={handleOrderNews}
+					filterOption={(input, option) =>
+						option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+					}
+					filterSort={(optionA, optionB) =>
+						optionA.children
+							.toLowerCase()
+							.localeCompare(optionB.children.toLowerCase())
+					}>
+					<Option value="default">Default</Option>
+					<Option value="asc">ASC</Option>
+					<Option value="desc">DESC</Option>
+				</Select>
+			</Space>
+		</Col>
+	);
+};
+
+export default Filter;
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,223 +1,160 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
-  Select,
-  Typography,
-  Row,
-  Col,
-  Avatar,
-  Card,
-  Skeleton,
-  Breadcrumb,
-  Button,
-  Space,
-} from "antd";
-import moment from "moment";
+	Typography,
+	Row,
+	Col,
+	Avatar,
+	Card,
+	Skeleton,
+	Breadcrumb,
+	Button,
+} from 'antd';
+import moment from 'moment';
+import Filter from '../components/Filter';
 
-import { useGetCryptosNewsQuery } from "../services/cryptoNewsApi";
-import { useGetCryptosQuery } from "../services/cryptoApi";
+import { useGetCryptosNewsQuery } from '../services/cryptoNewsApi';
 
-import demoImage from "../images/demo-image.jpeg";
+import demoImage from '../images/demo-image.jpeg';
 
 const { Text, Title } = Typography;
-const { Option } = Select;
 
 const News = ({ simplified }) => {
-  const [newsCategory, setNewsCategory] = useState("Crytocurrency");
-  const [sortBy, setSortBy] = useState("");
-  const [orderBy, setOrderBy] = useState("asc");
+	const [newsCategory, setNewsCategory] = useState('Crytocurrency');
+	const [sortBy, setSortBy] = useState('');
+	const [orderedNews, setOrderedNews] = useState('default');
 
-  const { data: cryptoNews } = useGetCryptosNewsQuery({
-    newsCategory,
-    count: simplified ? 4 : 24,
-    sortBy,
-  });
+	const { data: cryptoNews } = useGetCryptosNewsQuery({
+		newsCategory,
+		count: simplified ? 4 : 24,
+		sortBy,
+		orderedNews,
+	});
 
-  const { data } = useGetCryptosQuery(100);
-  const [visible, setVisible] = useState(8);
-  const [loading, setLoading] = useState(false);
+	const [visible, setVisible] = useState(8);
+	const [loading, setLoading] = useState(false);
 
-  const handleSelectNews = value => {
-    setNewsCategory(value);
-  };
+	const handleSelectNews = value => {
+		setNewsCategory(value);
+	};
 
-  const handleSortNews = value => {
-    setSortBy(value);
-  };
+	const handleSortNews = value => {
+		setSortBy(value);
+	};
 
-  const handleOrderNews = value => {
-    const isSelected = value;
-    console.log("C " + isSelected);
-    // console.log(
-    //   [].concat(cryptoNews.value).sort((a, b) => (a.name > b.name ? 1 : -1))
-    // );
+	const handleOrderNews = value => {
+		const orderByAsc = []
+			.concat(cryptoNews.value)
+			.sort((a, b) => (a.name > b.name ? 1 : -1));
 
-    const orderByAsc = []
-      .concat(cryptoNews.value)
-      .sort((a, b) => (a.name > b.name ? 1 : -1));
+		const orderByDesc = []
+			.concat(cryptoNews.value)
+			.sort((a, b) => (b.name < a.name ? -1 : 1));
 
-    const orderByDesc = []
-      .concat(cryptoNews.value)
-      .sort((a, b) => (b.name < a.name ? -1 : 1));
+		switch (value) {
+			case 'asc':
+				setOrderedNews(value);
+				console.log(orderByAsc); // array
+				return orderByAsc;
 
-    switch (value) {
-      case "asc":
-        setOrderBy(orderByAsc);
-        break;
+			case 'desc':
+				setOrderedNews(value);
+				break;
+			default:
+				setOrderedNews('default');
+				break;
+		}
+	};
 
-      case "desc":
-        setOrderBy(orderByDesc);
-        break;
-      default:
-        break;
-    }
-  };
+	console.log(`Sort: ${sortBy}`);
+	console.log(`Order: ${orderedNews}`);
 
-  const loadMore = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setVisible(visible + 4);
-      setLoading(false);
-    }, 1000);
-  };
+	const loadMore = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setVisible(visible + 4);
+			setLoading(false);
+		}, 1000);
+	};
 
-  if (!cryptoNews?.value) return <Skeleton active />;
+	if (!cryptoNews?.value) return <Skeleton active />;
 
-  return (
-    <>
-      <Row gutter={[24, 24]}>
-        {!simplified && (
-          <>
-            <Col>
-              <Title level={1} className="heading">
-                Crypto News
-              </Title>
-              <Breadcrumb>
-                <Breadcrumb.Item>
-                  <Link to="/">Home</Link>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>News</Breadcrumb.Item>
-              </Breadcrumb>
-            </Col>
-            <Col span={24}>
-              <Space size="small">
-                <Select
-                  className="select-news"
-                  placeholder="Select a category"
-                  optionFilterProp="children"
-                  showSearch
-                  onChange={handleSelectNews}
-                  filterOption={(inputValue, option) =>
-                    option.children
-                      .toLowerCase()
-                      .indexOf(inputValue.toLowerCase()) >= 0
-                  }
-                >
-                  <Option value="Cryptocurrency">All</Option>
-                  {data?.data?.coins.map(coin => (
-                    <Option value={coin.name} key={coin.id}>
-                      {coin.name}
-                    </Option>
-                  ))}
-                </Select>
-
-                <Select
-                  style={{ width: 200 }}
-                  placeholder="Sort by"
-                  optionFilterProp="children"
-                  onChange={handleSortNews}
-                  filterOption={(input, option) =>
-                    option.children
-                      .toLowerCase()
-                      .indexOf(input.toLowerCase()) >= 0
-                  }
-                  filterSort={(optionA, optionB) =>
-                    optionA.children
-                      .toLowerCase()
-                      .localeCompare(optionB.children.toLowerCase())
-                  }
-                >
-                  <Option value="relevance">Relevance</Option>
-                  <Option value="date">Date</Option>
-                </Select>
-
-                <Select
-                  style={{ width: 200 }}
-                  placeholder="Order by"
-                  optionFilterProp="children"
-                  onChange={handleOrderNews}
-                  filterOption={(input, option) =>
-                    option.children
-                      .toLowerCase()
-                      .indexOf(input.toLowerCase()) >= 0
-                  }
-                  filterSort={(optionA, optionB) =>
-                    optionA.children
-                      .toLowerCase()
-                      .localeCompare(optionB.children.toLowerCase())
-                  }
-                >
-                  <Option value="default">Default</Option>
-                  <Option value="asc">ASC</Option>
-                  <Option value="desc">DESC</Option>
-                </Select>
-              </Space>
-            </Col>
-          </>
-        )}
-        {cryptoNews.value.slice(0, visible).map((news, i) => (
-          <Col xs={24} md={12} xxl={6} key={i}>
-            <Card hoverable className="news-card">
-              <a href={news.url} target="_blank" rel="noreferrer">
-                <div className="news-image-container">
-                  <Title className="news-title" level={4}>
-                    {news.name}
-                  </Title>
-                  <img
-                    src={news?.image?.thumbnail?.contentUrl || demoImage}
-                    alt={news.name}
-                  />
-                </div>
-                <p>
-                  {news.description > 100
-                    ? `${news.description.substring(0, 100)}`
-                    : news.description}
-                </p>
-                <div className="provider-container">
-                  <div className="provider-meta">
-                    <Avatar
-                      src={
-                        news.provider[0]?.image?.thumbnail?.contentUrl ||
-                        demoImage
-                      }
-                      alt="news"
-                    />
-                    <Text className="provider-name">
-                      {news.provider[0]?.name}
-                    </Text>
-                    <Text italic={true} type="secondary">
-                      {moment(news.datePublished).startOf("ss").fromNow()}
-                    </Text>
-                  </div>
-                </div>
-              </a>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      {!simplified && (
-        <Row align="center">
-          {visible < cryptoNews?.value?.length && (
-            <div style={{ margin: "2rem 0" }}>
-              <Button loading={loading} onClick={loadMore}>
-                Load more
-              </Button>
-            </div>
-          )}
-        </Row>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Row gutter={[24, 24]}>
+				{!simplified && (
+					<>
+						<Col>
+							<Title level={1} className="heading">
+								Crypto News
+							</Title>
+							<Breadcrumb>
+								<Breadcrumb.Item>
+									<Link to="/">Home</Link>
+								</Breadcrumb.Item>
+								<Breadcrumb.Item>News</Breadcrumb.Item>
+							</Breadcrumb>
+						</Col>
+						<Filter
+							handleSelectNews={handleSelectNews}
+							handleSortNews={handleSortNews}
+							handleOrderNews={handleOrderNews}
+						/>
+					</>
+				)}
+				{cryptoNews.value.slice(0, visible).map((news, i) => (
+					<Col xs={24} md={12} xxl={6} key={i}>
+						<Card hoverable className="news-card">
+							<a href={news.url} target="_blank" rel="noreferrer">
+								<div className="news-image-container">
+									<Title className="news-title" level={4}>
+										{news.name}
+									</Title>
+									<img
+										src={news?.image?.thumbnail?.contentUrl || demoImage}
+										alt={news.name}
+									/>
+								</div>
+								<p>
+									{news.description > 100
+										? `${news.description.substring(0, 100)}`
+										: news.description}
+								</p>
+								<div className="provider-container">
+									<div className="provider-meta">
+										<Avatar
+											src={
+												news.provider[0]?.image?.thumbnail?.contentUrl ||
+												demoImage
+											}
+											alt="news"
+										/>
+										<Text className="provider-name">
+											{news.provider[0]?.name}
+										</Text>
+										<Text italic={true} type="secondary">
+											{moment(news.datePublished).startOf('ss').fromNow()}
+										</Text>
+									</div>
+								</div>
+							</a>
+						</Card>
+					</Col>
+				))}
+			</Row>
+			{!simplified && (
+				<Row align="center">
+					{visible < cryptoNews?.value?.length && (
+						<div style={{ margin: '2rem 0' }}>
+							<Button loading={loading} onClick={loadMore}>
+								Load more
+							</Button>
+						</div>
+					)}
+				</Row>
+			)}
+		</>
+	);
 };
 
 export default News;
