43467ef153c6338ad2437a70cabbf9df890e0c86
VasileSmartdata <vasile@smartdata.solutions>
2021-10-11 16:24:08 +0300

    feat: implemented toggle between list and grid for news page

--- src/components/News/NewsFilter.jsx
+++ src/components/News/NewsFilter.jsx
@@ -1,68 +1,98 @@
-import { Col, Space, Select } from 'antd';
-import { useGetCryptosQuery } from '../../services/cryptoApi';
+import { useState, useEffect } from "react";
+import { Col, Space, Select } from "antd";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
+
+import { ViewAs, ViewAsMobile } from "../";
 
 const { Option } = Select;
 
 const NewsFilter = ({ handleSelectNews, handleSortNews, handleOrderNews }) => {
-	const { data } = useGetCryptosQuery(100);
+  const { data } = useGetCryptosQuery(100);
+  const [resolution, setResolution] = useState(undefined);
 
-	return (
-		<Col span={24} className="news-filter-container">
-			<Space size="small">
-				<Select
-					className="select-news"
-					placeholder="Select a category"
-					optionFilterProp="children"
-					showSearch
-					onChange={handleSelectNews}
-					filterOption={(inputValue, option) =>
-						option.children.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0
-					}>
-					<Option value="Cryptocurrency">All</Option>
-					{data?.data?.coins.map(coin => (
-						<Option value={coin.name} key={coin.id}>
-							{coin.name}
-						</Option>
-					))}
-				</Select>
+  useEffect(() => {
+    const windowSize = () => setResolution(window.innerWidth);
 
-				<Select
-					style={{ width: 120 }}
-					placeholder="Sort by"
-					optionFilterProp="children"
-					onChange={handleSortNews}
-					filterOption={(input, option) =>
-						option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-					}
-					filterSort={(optionA, optionB) =>
-						optionA.children
-							.toLowerCase()
-							.localeCompare(optionB.children.toLowerCase())
-					}>
-					<Option value="relevance">Relevance</Option>
-					<Option value="date">Date</Option>
-				</Select>
+    window.addEventListener("resize", windowSize);
+    windowSize();
+    return () => {
+      window.removeEventListener("resize", windowSize);
+    };
+  }, []);
 
-				<Select
-					style={{ width: 120 }}
-					placeholder="Order by"
-					optionFilterProp="children"
-					onChange={handleOrderNews}
-					filterOption={(input, option) =>
-						option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-					}
-					filterSort={(optionA, optionB) =>
-						optionA.children
-							.toLowerCase()
-							.localeCompare(optionB.children.toLowerCase())
-					}>
-					<Option value="default">Default</Option>
-					<Option value="asc">ASC</Option>
-					<Option value="desc">DESC</Option>
-				</Select>
-			</Space>
-		</Col>
-	);
+  useEffect(() => {
+    if (resolution < 576) {
+      <ViewAsMobile />;
+    } else {
+      <ViewAs />;
+    }
+  }, [resolution]);
+
+  return (
+    <>
+      <Col span={20} xs={24} sm={18} className="news-filter-container">
+        <Space size="small">
+          <Select
+            className="select-news"
+            placeholder="Select a category"
+            optionFilterProp="children"
+            showSearch
+            onChange={handleSelectNews}
+            filterOption={(inputValue, option) =>
+              option.children.toLowerCase().indexOf(inputValue.toLowerCase()) >=
+              0
+            }
+          >
+            <Option value="Cryptocurrency">All</Option>
+            {data?.data?.coins.map(coin => (
+              <Option value={coin.name} key={coin.id}>
+                {coin.name}
+              </Option>
+            ))}
+          </Select>
+
+          <Select
+            style={{ width: 120 }}
+            placeholder="Sort by"
+            optionFilterProp="children"
+            onChange={handleSortNews}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+            filterSort={(optionA, optionB) =>
+              optionA.children
+                .toLowerCase()
+                .localeCompare(optionB.children.toLowerCase())
+            }
+          >
+            <Option value="relevance">Relevance</Option>
+            <Option value="date">Date</Option>
+          </Select>
+
+          <Select
+            style={{ width: 120 }}
+            placeholder="Order by"
+            optionFilterProp="children"
+            onChange={handleOrderNews}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+            filterSort={(optionA, optionB) =>
+              optionA.children
+                .toLowerCase()
+                .localeCompare(optionB.children.toLowerCase())
+            }
+          >
+            <Option value="default">Default</Option>
+            <Option value="asc">ASC</Option>
+            <Option value="desc">DESC</Option>
+          </Select>
+        </Space>
+      </Col>
+
+      {resolution < 576 ? <ViewAsMobile /> : <ViewAs />}
+    </>
+  );
 };
 
 export default NewsFilter;
---
+++ src/components/ViewAs/ViewAs.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { AppstoreOutlined, MenuOutlined } from "@ant-design/icons";
+import { Button, Col, Space, Tooltip } from "antd";
+
+import { ViewAsContext } from "../../app/context/viewAsContext";
+
+const ViewAs = () => {
+  const { view, setView } = useContext(ViewAsContext);
+
+  return (
+    <Col span={4} xs={24} sm={6} style={{ paddingRight: 0 }}>
+      <Space size="small" className="view-as">
+        <Tooltip placement="top" title="View as list">
+          <Button
+            onClick={() => setView("list")}
+            data-list={view === "list" ? "list" : null}
+          >
+            <MenuOutlined />
+          </Button>
+        </Tooltip>
+        <Tooltip placement="top" title="View as grid">
+          <Button
+            onClick={() => setView("grid")}
+            data-list={view === "grid" ? "grid" : null}
+          >
+            <AppstoreOutlined />
+          </Button>
+        </Tooltip>
+      </Space>
+    </Col>
+  );
+};
+
+export default ViewAs;
