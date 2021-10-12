311965d9763f90b84090078dbc845d112bf71385
VasileSmartdata <vasile@smartdata.solutions>
2021-10-12 16:42:35 +0300

    coinranking api v2

--- src/components/Cryptocurrency/CryptocurrenciesContainer.jsx
+++ src/components/Cryptocurrency/CryptocurrenciesContainer.jsx
@@ -1,20 +1,23 @@
-import { Row } from 'antd';
-import { CryptocurrenciesCard, EmptyResults } from '../';
+import { Row } from "antd";
+import { CryptocurrenciesCard, EmptyResults } from "../";
 
 const CryptocurrenciesContainer = ({ data, visible }) => {
-	return (
-		<Row gutter={[24, 24]} className="crypto-card-container">
-			{data?.length >= 2 ? (
-				data
-					?.slice(0, visible)
-					.map(currency => (
-						<CryptocurrenciesCard currency={currency} key={currency.id} />
-					))
-			) : (
-				<EmptyResults />
-			)}
-		</Row>
-	);
+    return (
+        <Row gutter={[24, 24]} className="crypto-card-container">
+            {data?.length >= 2 ? (
+                data
+                    ?.slice(0, visible)
+                    .map(currency => (
+                        <CryptocurrenciesCard
+                            currency={currency}
+                            key={currency.uuid}
+                        />
+                    ))
+            ) : (
+                <EmptyResults />
+            )}
+        </Row>
+    );
 };
 
 export default CryptocurrenciesContainer;
