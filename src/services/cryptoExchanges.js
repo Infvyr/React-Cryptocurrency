109d7f0a9e73c238cafbdeaa4d382474052074f2
VasileSmartdata <vasile@smartdata.solutions>
2021-10-12 14:37:28 +0300

    test api

--- src/services/cryptoExchanges.js
+++ src/services/cryptoExchanges.js
@@ -1,22 +1,22 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoApiHeaders = {
-	'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-	'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
-};
+// const cryptoApiHeaders = {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     // "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+// };
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const baseUrl = "https://api.coinranking.com/v2";
 
-const createRequest = url => ({ url, headers: cryptoApiHeaders });
+const createRequest = url => ({ url });
 
 export const cryptoExchanges = createApi({
-	reducerPath: 'cryptoExchanges',
-	baseQuery: fetchBaseQuery({ baseUrl }),
-	endpoints: builder => ({
-		getExchanges: builder.query({
-			query: () => createRequest('/exchanges'),
-		}),
-	}),
+    reducerPath: "cryptoExchanges",
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: builder => ({
+        getExchanges: builder.query({
+            query: () => createRequest("/exchanges"),
+        }),
+    }),
 });
 
 export const { useGetExchangesQuery } = cryptoExchanges;
