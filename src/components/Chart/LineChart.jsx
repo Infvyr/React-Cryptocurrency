fb641c43532b9d893aa7aa32681a68a87160fd98
Infvyr <allfortrashmail@gmail.com>
2021-10-12 18:12:11 +0300

    minor fixes

--- src/components/Chart/LineChart.jsx
+++ src/components/Chart/LineChart.jsx
@@ -1,59 +1,65 @@
-import { Line } from "react-chartjs-2";
-import { Col, Row, Typography } from "antd";
-
-const { Title } = Typography;
+import { Line } from 'react-chartjs-2';
+import { Col, Row } from 'antd';
+import { HeadingTitle } from '../';
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-  const cointPrice = [];
-  const coinTimestamp = [];
+	const cointPrice = [];
+	const coinTimestamp = [];
 
-  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    cointPrice.push(coinHistory.data.history[i].price);
-    coinTimestamp.push(
-      new Date(coinHistory.data.history[i].timestamp).toLocaleDateString()
-    );
-  }
+	for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+		cointPrice.push(coinHistory.data.history[i].price);
+		coinTimestamp.push(
+			new Date(coinHistory.data.history[i].timestamp).toLocaleDateString()
+		);
+	}
 
-  const data = {
-    labels: coinTimestamp,
-    datasets: [
-      {
-        label: "Price in USD",
-        data: cointPrice,
-        fill: false,
-        backgroundColor: "#0071bd",
-        borderColor: "#0071bd",
-      },
-    ],
-  };
+	const data = {
+		labels: coinTimestamp,
+		datasets: [
+			{
+				label: 'Price in USD',
+				data: cointPrice,
+				fill: false,
+				backgroundColor: '#0071bd',
+				borderColor: '#0071bd',
+			},
+		],
+	};
 
-  const options = {
-    scales: {
-      yAxes: [{ ticks: { beginAtZero: true } }],
-    },
-  };
+	const options = {
+		scales: {
+			yAxes: [{ ticks: { beginAtZero: true } }],
+		},
+	};
 
-  return (
-    <>
-      <Row className="chart-header">
-        <Title level={2} className="chart-title">
-          {coinName} Price chart
-        </Title>
-        <Col className="price-container">
-          {coinHistory?.data?.change && (
-            <Title level={5} className="price-change">
-              {coinHistory?.data?.change}%
-            </Title>
-          )}
+	return (
+		<>
+			<Row className="chart-header">
+				<HeadingTitle
+					levelNumber={2}
+					cssClass="chart-title"
+					title={`${coinName} Price chart`}
+				/>
 
-          <Title level={5} className="current-price">
-            Current {coinName} Price: $ {currentPrice}
-          </Title>
-        </Col>
-      </Row>
-      <Line data={data} options={options}></Line>
-    </>
-  );
+				<Col className="price-container">
+					{coinHistory?.data?.change && (
+						<HeadingTitle
+							levelNumber={5}
+							title={`${coinHistory?.data?.change}%`}
+							cssClass="price-change"
+						/>
+					)}
+
+					<HeadingTitle
+						levelNumber={5}
+						title={`Current ${coinName} Price: $ ${currentPrice}`}
+						cssClass="current-price"
+					/>
+				</Col>
+			</Row>
+			<Line data={data} options={options}></Line>
+		</>
+	);
 };
 
 export default LineChart;
--- src/components/ViewAs/ViewAs.jsx
+++ src/components/ViewAs/ViewAs.jsx
@@ -1,34 +1,33 @@
-import { useContext } from "react";
-import { AppstoreOutlined, MenuOutlined } from "@ant-design/icons";
-import { Button, Col, Space, Tooltip } from "antd";
+import { useContext } from 'react';
+import { AppstoreOutlined, MenuOutlined } from '@ant-design/icons';
+import { Button, Col, Space, Tooltip } from 'antd';
 
-import { ViewAsContext } from "../../app/context/viewAsContext";
+import { ViewAsContext } from '../../app/context/viewAsContext';
 
 const ViewAs = () => {
-  const { view, setView } = useContext(ViewAsContext);
+	const { view, setView } = useContext(ViewAsContext);
 
-  return (
-    <Col span={4} xs={24} sm={6} style={{ paddingRight: 0 }}>
-      <Space size="small" className="view-as">
-        <Tooltip placement="top" title="View as list">
-          <Button
-            onClick={() => setView("list")}
-            data-list={view === "list" ? "list" : null}
-          >
-            <MenuOutlined />
-          </Button>
-        </Tooltip>
-        <Tooltip placement="top" title="View as grid">
-          <Button
-            onClick={() => setView("grid")}
-            data-list={view === "grid" ? "grid" : null}
-          >
-            <AppstoreOutlined />
-          </Button>
-        </Tooltip>
-      </Space>
-    </Col>
-  );
+	return (
+		<Col span={4} xs={24} sm={6} style={{ paddingRight: 0 }}>
+			<Space size="small" className="view-as">
+				<Tooltip placement="top" title="View as list">
+					<Button
+						onClick={() => setView('list')}
+						data-list={view === 'list' ? 'list' : null}>
+						<MenuOutlined />
+					</Button>
+				</Tooltip>
+				<Tooltip placement="top" title="View as grid">
+					<Button
+						onClick={() => setView('grid')}
+						data-list={view === 'grid' ? 'grid' : null}>
+						<AppstoreOutlined />
+					</Button>
+				</Tooltip>
+			</Space>
+			<div style={{ height: '30px' }} />
+		</Col>
+	);
 };
 
 export default ViewAs;
