c649150e857fcc94e57cb85da63a05602b067edb
VasileSmartdata <vasile@smartdata.solutions>
2021-10-13 12:40:06 +0300

    fix: Cryptocurrencies filter

--- src/components/Cryptocurrency/CryptocurrenciesContainer.jsx
+++ src/components/Cryptocurrency/CryptocurrenciesContainer.jsx
@@ -1,24 +1,24 @@
-import { Row } from 'antd';
-import { CryptocurrenciesCard, EmptyResults } from '../';
+import { Row } from "antd";
+import { CryptocurrenciesCard, EmptyResults } from "../";
 
 const CryptocurrenciesContainer = ({ data, visible, filterBy }) => {
-	return (
-		<Row gutter={[24, 24]} className="crypto-card-container">
-			{data?.length >= 2 ? (
-				data
-					?.slice(0, visible)
-					.map(currency => (
-						<CryptocurrenciesCard
-							currency={currency}
-							key={currency.uuid}
-							filterBy={filterBy}
-						/>
-					))
-			) : (
-				<EmptyResults />
-			)}
-		</Row>
-	);
+    return (
+        <Row gutter={[24, 24]} className="crypto-card-container">
+            {data?.length >= 2 ? (
+                data
+                    ?.slice(0, visible)
+                    .map(currency => (
+                        <CryptocurrenciesCard
+                            currency={currency}
+                            key={currency.id}
+                            filterBy={filterBy}
+                        />
+                    ))
+            ) : (
+                <EmptyResults />
+            )}
+        </Row>
+    );
 };
 
 export default CryptocurrenciesContainer;
--- src/pages/Cryptocurrencies.jsx
+++ src/pages/Cryptocurrencies.jsx
@@ -1,140 +1,167 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Col, Skeleton, Breadcrumb } from 'antd';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Col, Skeleton, Breadcrumb } from "antd";
 
 import {
-	HeadingTitle,
-	Search,
-	CryptocurrenciesFilter,
-	CryptocurrenciesContainer,
-	LoadMore,
-} from '../components';
+    HeadingTitle,
+    Search,
+    CryptocurrenciesFilter,
+    CryptocurrenciesContainer,
+    LoadMore,
+} from "../components";
 
-import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
 const Cryptocurrencies = ({ simplified }) => {
-	const count = simplified ? 10 : 100;
-	const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
-	const [cryptos, setCryptos] = useState([]);
-	const [searchTerm, setSearchTerm] = useState('');
-	const [visible, setVisible] = useState(simplified ? 10 : 12);
-	const [isFilterBySelected, setIsFilterBySelected] = useState(false);
-	const [filterVal, setFilterVal] = useState(undefined);
+    const count = simplified ? 10 : 100;
+    const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [visible, setVisible] = useState(simplified ? 10 : 12);
+    const [isFilterBySelected, setIsFilterBySelected] = useState(false);
+    const [filterVal, setFilterVal] = useState(undefined);
 
-	const filterValuesArr = ['price', 'marketCap', 'change'];
+    const filterValuesArr = ["price", "change"];
 
-	useEffect(() => {
-		const filteredData = cryptoList?.data?.coins.filter(coin =>
-			coin.name.toLowerCase().includes(searchTerm.toLowerCase())
-		);
+    useEffect(() => {
+        const filteredData = cryptoList?.data?.coins.filter(coin =>
+            coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
 
-		setCryptos(filteredData);
-	}, [cryptoList, searchTerm]);
+        setCryptos(filteredData);
+    }, [cryptoList, searchTerm]);
 
-	// handle search
-	const handleSearch = e => setSearchTerm(e.target.value);
+    // handle search
+    const handleSearch = e => setSearchTerm(e.target.value);
 
-	// handle the filter dropdown by
-	const handleFilterBy = value => {
-		switch (value) {
-			case 'price':
-			case 'marketCap':
-			case 'change':
-				setIsFilterBySelected(true);
-				setFilterVal(value);
-				break;
+    // handle the filter dropdown by
+    const handleFilterBy = value => {
+        switch (value) {
+            case "price":
+            case "marketCap":
+            case "change":
+                setIsFilterBySelected(true);
+                setFilterVal(value);
+                break;
 
-			default:
-				setIsFilterBySelected(false);
-				setFilterVal('price');
-				break;
-		}
-	};
+            default:
+                setIsFilterBySelected(false);
+                setFilterVal("price");
+                break;
+        }
+    };
 
-	// handle the filter dropdown by
-	const handleOrderBy = value => {
-		switch (value) {
-			case 'asc':
-				filterValuesArr.forEach(val => {
-					if (filterVal === val) {
-						setCryptos(
-							[].concat(cryptos).sort((a, b) => {
-								return parseFloat(a.val) > parseFloat(b.val) ? 1 : -1;
-							})
-						);
-					}
-				});
-				break;
+    // JSON.stringify(+a[val]) > JSON.stringify(+b[val]) ? 1 : -1;
 
-			case 'desc':
-				filterValuesArr.forEach(val => {
-					if (filterVal === val) {
-						setCryptos(
-							[].concat(cryptos).sort((a, b) => {
-								return parseFloat(a.val) > parseFloat(b.val) ? 1 : -1;
-							})
-						);
-					}
-				});
-				break;
+    // handle the filter dropdown by
+    const handleOrderBy = value => {
+        switch (value) {
+            case "asc":
+                filterValuesArr.forEach(val => {
+                    if (filterVal === val) {
+                        setCryptos(
+                            []
+                                .concat(cryptos)
+                                .sort((a, b) =>
+                                    parseFloat(a[val]) > parseFloat(b[val])
+                                        ? 1
+                                        : -1
+                                )
+                        );
+                    }
 
-			default:
-				setCryptos([].concat(cryptos));
-				break;
-		}
-	};
+                    if (filterVal === "marketCap") {
+                        setCryptos(
+                            []
+                                .concat(cryptos)
+                                .sort((a, b) => a.marketCap - b.marketCap)
+                        );
+                    }
+                });
 
-	if (isFetching) return <Skeleton active />;
+                break;
 
-	return (
-		<>
-			{!simplified && (
-				<>
-					<Col>
-						<HeadingTitle
-							levelNumber={1}
-							title="Cryptocurrencies"
-							cssClass="heading"
-						/>
-						<Breadcrumb>
-							<Breadcrumb.Item>
-								<Link to="/">Home</Link>
-							</Breadcrumb.Item>
-							<Breadcrumb.Item>Cryptocurrencies</Breadcrumb.Item>
-						</Breadcrumb>
-					</Col>
-					<Col className="cryptocurrency-filter">
-						<Search
-							placeholder="Search Cryptocurrency"
-							size="large"
-							cssClass="search-crypto"
-							onChange={handleSearch}
-						/>
-						<CryptocurrenciesFilter
-							handleFilterBy={handleFilterBy}
-							handleOrderBy={handleOrderBy}
-							isFilterBySelected={isFilterBySelected}
-						/>
-					</Col>
-				</>
-			)}
+            case "desc":
+                filterValuesArr.forEach(val => {
+                    if (filterVal === val) {
+                        setCryptos(
+                            []
+                                .concat(cryptos)
+                                .sort((a, b) =>
+                                    parseFloat(b[val]) < parseFloat(a[val])
+                                        ? -1
+                                        : 1
+                                )
+                        );
+                    }
 
-			<CryptocurrenciesContainer
-				data={cryptos}
-				visible={visible}
-				filterBy={filterVal}
-			/>
+                    if (filterVal === "marketCap") {
+                        setCryptos(
+                            []
+                                .concat(cryptos)
+                                .sort((a, b) => b.marketCap - a.marketCap)
+                        );
+                    }
+                });
+                break;
 
-			{!simplified && (
-				<LoadMore
-					visible={visible}
-					data={cryptos}
-					setVisible={setVisible}
-					loadAmount={6}
-				/>
-			)}
-		</>
-	);
+            default:
+                setCryptos([].concat(cryptos));
+                break;
+        }
+    };
+
+    if (isFetching) return <Skeleton active />;
+
+    return (
+        <>
+            {!simplified && (
+                <>
+                    <Col>
+                        <HeadingTitle
+                            levelNumber={1}
+                            title="Cryptocurrencies"
+                            cssClass="heading"
+                        />
+                        <Breadcrumb>
+                            <Breadcrumb.Item>
+                                <Link to="/">Home</Link>
+                            </Breadcrumb.Item>
+                            <Breadcrumb.Item>Cryptocurrencies</Breadcrumb.Item>
+                        </Breadcrumb>
+                    </Col>
+                    <Col className="cryptocurrency-filter">
+                        <Search
+                            placeholder="Search Cryptocurrency"
+                            size="large"
+                            cssClass="search-crypto"
+                            onChange={handleSearch}
+                        />
+                        <CryptocurrenciesFilter
+                            handleFilterBy={handleFilterBy}
+                            handleOrderBy={handleOrderBy}
+                            isFilterBySelected={isFilterBySelected}
+                        />
+                    </Col>
+                </>
+            )}
+
+            <CryptocurrenciesContainer
+                data={cryptos}
+                visible={visible}
+                filterBy={filterVal}
+            />
+
+            {!simplified && (
+                <LoadMore
+                    visible={visible}
+                    data={cryptos}
+                    setVisible={setVisible}
+                    loadAmount={6}
+                />
+            )}
+        </>
+    );
 };
 
 export default Cryptocurrencies;
